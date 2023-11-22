0a27fd693e10360fe119b91f080432bfb5d31b88
abdur-rafi <abdurrafi403@gmail.com>
2023-11-23 00:56:25 +0600

    random eigen

---
+++ random_eigen.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def generateInvertibleMatrix(n: int):
+    A = np.random.randint(5, size=(n, n))
+    rowSumSubDiag = np.sum(np.abs(A) , axis=1) - np.abs(np.diag(A))
+    # print(rowSumSubDiag) 
+    np.fill_diagonal(A, rowSumSubDiag + 1)
+    return A
+
+
+def main():
+    n = 3
+    mat = generateInvertibleMatrix(n)
+    print("Matrix A:")
+    print(mat)
+
+    eigenvalues, eigenvectors = np.linalg.eig(mat)
+    # print("Eigenvalues:")
+    # print(eigenvalues)
+    # print("Eigenvectors:")
+    # print(eigenvectors)
+
+    V = eigenvectors
+    Diaglmbda = np.diag(eigenvalues)
+
+    matReconstructed = np.matmul(np.matmul(V, Diaglmbda), np.linalg.inv(V))
+
+    print("Reconstructed Matrix: ")
+    print(matReconstructed)
+
+
+if __name__ == "__main__":
+    main()
+
