021e6d5768d0e9e70b9fd0725d09bbc63df901de
abdur-rafi <abdurrafi403@gmail.com>
2024-01-02 16:00:18 +0600

    submission

---
+++ 1805008/test_1805008.py
@@ -0,0 +1,28 @@
+import pickle
+from train_1805008 import Input,Dense,Softmax,Relu,DropOut, Model, Data, prepareData, acc, macroF1
+
+def loadModel(path):
+    with open(path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+
+
+def main():
+    path = 'model_1805008.pickle'
+    model = loadModel(path)
+    data = prepareData()
+    tsX = data.tsX
+    tsY = data.tsY
+    predY = model.predict(tsX)
+    print(f"Test Accuracy: {acc(model, tsX, tsY)}")
+    print(f"Test Macro F1: {macroF1(model, tsX, tsY)}")
+    cm = confusionMatrix(model, data.vX, data.vY)
+    plotConfusionMatrix(cm, "Validation Confusion Matrix")
+    cm = confusionMatrix(model, tsX, tsY)
+    plotConfusionMatrix(cm, "Test Set Confusion Matrix")
+
+if __name__ == '__main__':
+    main()
+
+    # print(model)
